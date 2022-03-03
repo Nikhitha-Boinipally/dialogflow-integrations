4e9f3a40ed79012e2ffd2425c7dc10f51f45866a
Nikhitha-Boinipally <99815792+Nikhitha-Boinipally@users.noreply.github.com>
2022-03-03 10:17:04 +0530

    Update server.js

--- twilio/server.js
+++ twilio/server.js
@@ -29,10 +29,10 @@ app.use(bodyParser.urlencoded({ extended: true }));
 //See https://dialogflow.com/docs/reference/v2-auth-setup and
 // https://cloud.google.com/dialogflow/docs/setup for details.
 
-const projectId = 'Place your dialogflow projectId here';
-const phoneNumber = "Place your twilio phone number here";
-const accountSid = 'Place your accountSid here';
-const authToken = 'Place your authToken here';
+const projectId = 'elated-drive-339510';
+const phoneNumber = "+(681) 491-4412";
+const accountSid = 'AC3a39dbc2b5f215193a2bd8cb72fd84e3';
+const authToken = '38e02b5c6b09c7cd72252ea6a006e4fb';
 
 const client = require('twilio')(accountSid, authToken);
 const MessagingResponse = require('twilio').twiml.MessagingResponse;
